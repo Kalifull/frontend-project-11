00137c112c0a1798cc5a148582dba7b5c43e9762
Alexey <kalifull@mail.ru>
2022-08-30 04:36:34 +0300

    add messages for i18next library

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    form: {
+      errorMessages: {
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        requiredField: 'Не должно быть пустым',
+        duplicateUrl: 'RSS уже существует',
+      },
+      successMessages: {
+        validUrl: 'RSS успешно загружен',
+      },
+    },
+  },
+};
