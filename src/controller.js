aad34dcfdc0f6dd8205ece230ae89b022f490fb5
Alexey <kalifull@mail.ru>
2022-09-02 12:32:43 +0300

    add displayData func

---
+++ src/controller.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import parse from './utils/parser.js';
+import buildRequest from './utils/buildRequest.js';
+import makePostList from './utils/makePostList.js';
+
+const routes = {
+  usersPath: (url, watchedState) => buildRequest(url, watchedState),
+};
+
+export default async (url, watchedState) => {
+  try {
+    const response = await axios.get(routes.usersPath(url, watchedState));
+
+    const data = parse(response, url, watchedState);
+
+    const { channel, items } = data;
+
+    makePostList(channel, items, watchedState);
+  } catch (error) {
+    watchedState.loadingProcess.status = 'failed';
+    watchedState.loadingProcess.loadingProcessError = error;
+  }
+};
