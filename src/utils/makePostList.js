baf01364574930aacb72f121009e605e2ef1d1a3
Alexey <kalifull@mail.ru>
2022-09-02 12:37:19 +0300

    add makePostList func

---
+++ src/utils/makePostList.js
@@ -0,0 +1,15 @@
+import { uniqueId } from 'lodash';
+
+export default (channel, items, watchedState) => {
+  channel.id = uniqueId();
+
+  const posts = items.map((item) => Object.assign(item, {
+    feedId: channel.id,
+    postId: uniqueId(),
+  }));
+
+  watchedState.posts.unshift(...posts);
+
+  watchedState.loadingProcess.status = 'idle';
+  watchedState.form.processState = 'loaded';
+};
