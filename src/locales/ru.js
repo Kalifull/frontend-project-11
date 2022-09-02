db2dedae705b5d3d8b0146a97c89d938a6e59eac
Alexey <kalifull@mail.ru>
2022-09-02 09:37:48 +0300

    Update translation

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -6,10 +6,15 @@ export default {
         invalidUrl: 'Ссылка должна быть валидным URL',
         requiredField: 'Поле не должно быть пустым',
         duplicateUrl: 'RSS уже существует',
+        networkError: 'Ошибка сети',
       },
-      successMessages: {
+      successMessage: {
         validUrl: 'RSS успешно загружен',
       },
+    },
+    loadingProcess: {
+      loading: 'Идет загрузка RSS. Пожалуйста, подождите...',
+      failed: 'Упс... что-то пошло не так =(. Попробуйте еще раз',
     },
   },
 };
