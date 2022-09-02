567cf5ff31772a9b37c13d0016991a18a7d22870
Alexey <kalifull@mail.ru>
2022-09-02 12:30:52 +0300

    code refactoring

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
-import axios from 'axios';
 import i18n from 'i18next';
 import onChange from 'on-change';
 import isEmpty from 'lodash/isEmpty.js';
 
-import parse from './utils/parser.js';
 import initView from './render/view.js';
 import validate from './utils/validate.js';
-import buildUrl from './utils/buildUrl.js';
 import resources from './locales/index.js';
-
-const routes = {
-  usersPath: (url) => buildUrl(url),
-};
+import displayData from './controller.js';
 
 export default async () => {
   const defaultLanguage = 'ru';
@@ -63,30 +57,22 @@ export default async () => {
     const formData = new FormData(event.target);
     const url = formData.get('url').trim();
 
-    watchedState.form.processState = 'sending';
+    watchedState.loadingProcess.loadingProcessError = null;
+    watchedState.loadingProcess.status = 'idle';
 
-    try {
-      const validationError = await validate(url, watchedState.feeds, i18nInstance);
+    const validationError = await validate(url, watchedState, i18nInstance);
 
-      watchedState.form.valid = isEmpty(validationError);
-      watchedState.form.processStateError = validationError;
+    watchedState.form.valid = isEmpty(validationError);
+    watchedState.form.processStateError = validationError;
 
-      if (!watchedState.form.valid) {
-        watchedState.form.processState = 'failed';
-        return;
-      }
-
-      const response = await axios.get(routes.usersPath(url));
-      watchedState.form.processState = 'loaded';
-
-      const data = parse(response);
-      watchedState.feeds.push({ url });
-      console.log(data);
-    } catch (errors) {
+    if (!watchedState.form.valid) {
       watchedState.form.processState = 'failed';
-      watchedState.form.processStateError = errors;
-    } finally {
-      watchedState.form.processState = 'filling';
+      console.log(watchedState);
+      return;
     }
+
+    await displayData(url, watchedState);
+    watchedState.form.processState = 'filling';
+    console.log(watchedState);
   });
 };
