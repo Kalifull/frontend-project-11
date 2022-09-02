777d7609d88b8f6cf2022cd8dc18c044c332bbed
Alexey <kalifull@mail.ru>
2022-09-02 12:45:22 +0300

    fix name func

--- src/utils/buildUrl.js
+++
@@ -1,9 +0,0 @@
-export default (url) => {
-  const proxy = 'https://allorigins.hexlet.app';
-  const parsedUrl = new URL('get', proxy);
-
-  parsedUrl.searchParams.append('disableCache', true);
-  parsedUrl.searchParams.append('url', url);
-
-  return parsedUrl.href;
-};
