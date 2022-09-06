4330dff103ff1ddcc5a1efb523644acb7918255b
Alexey <kalifull@mail.ru>
2022-09-06 17:15:44 +0300

    add en.js

---
+++ src/locales/en.js
@@ -0,0 +1,21 @@
+export default {
+  translation: {
+    form: {
+      errorMessages: {
+        notContainValidRss: 'Ресурс не содержит валидный RSS',
+        invalidUrl: 'Link must be a valid URL',
+        requiredField: 'The field must not be empty',
+        duplicateUrl: 'RSS already exists',
+        networkError: 'Network error',
+      },
+    },
+    loadingProcess: {
+      idle: 'RSS uploaded successfully',
+      loading: 'RSS is loading. Wait, please...',
+      failed: 'Oops... something went wrong =(. Please try again',
+    },
+    feeds: 'Feeds',
+    posts: 'Posts',
+    previewButton: 'View',
+  },
+};
